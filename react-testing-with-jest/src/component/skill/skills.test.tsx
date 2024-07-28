dcce8954fdeaf3c352cb6facd3c7126b503bb632
Jonathan Yan <jonathan.yan10@googlemail.com>
2024-07-28 10:35:49 +0100

    React Testing Tutorial 28

---
+++ react-testing-with-jest/src/component/skill/skills.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { Skills } from "./skills";
+
+describe('Skills', () => {
+    const skills = ['HTML', 'CSS', 'JavaScript'];
+
+    test('renders correctly', () => {
+        render(<Skills skills={skills} />)
+        const listElement = screen.getByRole('list');
+        expect(listElement).toBeInTheDocument();
+    })
+
+    test('renders a list of skills', () => {
+        render(<Skills skills={skills}/>)
+        // Will fail if using getByRole because multiple elements will be returned.
+        const listItemsElements = screen.getAllByRole('listitem');
+        // Checks the test data quantity is being rendered
+        expect(listItemsElements).toHaveLength(skills.length);
+    })
+})
