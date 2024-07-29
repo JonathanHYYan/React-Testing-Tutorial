985753e38fb46497bad1e40402d62a8651fb1394
Jonathan Yan <jonathan.yan10@googlemail.com>
2024-07-29 23:08:17 +0100

    React Testing Tutorial 38

--- react-testing-with-jest/src/component/mui/mui-mode.test.tsx
+++ react-testing-with-jest/src/component/mui/mui-mode.test.tsx
@@ -1,12 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../test-utils";
 import { MuiMode } from "./mui-mode";
-import { AppProviders } from "../../providers/app-providers";
 
 describe('MuiMode', () => {
     test('renders mode text correctly', () => {
-        render(<MuiMode/>, {
-            wrapper: AppProviders,
-        });
+        render(<MuiMode/>);
         const headerElement = screen.getByRole('heading');
         expect(headerElement).toHaveTextContent('dark mode');
     });
